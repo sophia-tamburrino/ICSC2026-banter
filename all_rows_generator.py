e8a98b066fd7059c6e693569c42ea9cfbf3c29b4
Sophia Tamburrino <sophia.tamburrino>
2026-01-31 15:10:00 +0000

    cleaning up comments and structure

--- all_rows_generator.py
+++ all_rows_generator.py
@@ -8,148 +8,94 @@ from matplotlib import pyplot as plt
 import numpy as np
 import random
 
+# This file will go through notmaturecorrect.csv and notmaturecorrect_meta.csv to grab pairs of dialogue that match our requirements. 
+
+# The get_pairs function takes the scraped data and metadata and goes through the dataframe line-by-line to pull a "dialogue" and "response".
+# Notes: In order to be considered valid dialogue, it must have at least two double quotation marks (one to begin and one to end) within the line. 
+# Additionally, to be considered a valid response to that dialogue, it must not be more than 2 lines ahead of it.
+# Dependencies: Reads notmaturecorrect.csv and notmaturecorrect_meta.csv and outputs the pairs and pairs_meta lists.
+# Arguments: body_path is the 'notmaturecorrect.csv' file, and meta_path is the 'notmaturecorrect_meta.csv' file. 
+
 def get_pairs(body_path, meta_path):
 
     # create dialogue + meta data list
     pairs = [["Dialogue", "Response"]]
-    
-    # pairs_meta = [["Number", "Dialogue", "Response", "URL", "hash", "num_pairs", "work_id", "author", 
-    #          "rating", "category", "fandom", "relationship", 
-    #          "character", "additional tags", "language", 
-    #          "published", "status", "status date",
-    #          "words", "chapters", "comments", "kudos", "bookmarks",
-    #          "hits", "all_kudos"]]
-
-    #'work_id': novel, 'body': text, 'author': author, 'title': title,'words': wordcount, 'fandom': fandom, 'category': category
     pairs_meta = [["Number", "Dialogue", "Response", "URL", "hash", "num_pairs", "work_id", "title", "author", "fandom", "words","category"]]
 
     # save csv to a dataframe
     body_df = pd.read_csv(body_path, encoding="cp1252")
     meta_df = pd.read_csv(meta_path, header=None, names=['work_id', 'url'], encoding="cp1252")
     num = 1
+
     # iterate through each row (novel) in the csv
     for i, row in body_df.iterrows():
 
-        if (row['work_id'] != 6848920) and (row['work_id'] != 35255137) and (row['work_id'] != 6953299) and (row['work_id'] != 17745707) and (row['work_id'] != 20062966):
-            # get the title
-            #title = row['title']
-            # replace illegal characters to fix filename generation
-            #title = title.replace("/", "-")
-            #title = title.replace("\"", "") #DEBUG STATEMENT - Sophia
-            #title = title.replace("?", "") #DEBUG STATEMENT - Sophia
-            # create a text file to store the body text in
+        id = row['work_id']
+        dir = "body-text/"
+        filename = dir + str(id) + "_body.txt"
+        text = row["body"]  
 
-            # replace title with work id for now - Jess
-            id = row['work_id']
-            print(i, id) #DEBUG STATEMENT - Jess
-            dir = "body-text/"
-            filename = dir + str(id) + "_body.txt"
-            #print(filename) DEBUG STATEMENT - Sophia
-            text = row["body"]  
-            #text = row["words"]
+        if not os.path.exists(dir):
+            os.makedirs(dir)
 
-            if not os.path.exists(dir):
-                os.makedirs(dir)
+        # save the text to a .txt file  
+        with open(filename, "w") as w:
+            w.write(str(text)) 
 
-            # save the text to a .txt file  
-            with open(filename, "w") as w:
-                #print(type(text)) #DEBUG - Jess
-                w.write(str(text)) ##DEBUG - had to cast text to str?? (Jess)
+        # remove whitespaces to allow for easy comparison of lines
+        clean_text = filename + "_clean"
+        with open(filename, 'r') as r, open (clean_text, 'w') as o:
+            for line in r:
+                if line.strip():
+                    o.write(line)
+        
+        # read line by line and check for dialogue pairs
+        with open(clean_text, 'r') as file:
+            # save lines to a list
+            lines = file.readlines()
+            temp_pairs = ["temp"]
+            pairs_count = -1
+            for index, line in enumerate(lines):
+                # make sure lines are in bounds 
+                if index + 1 < len(lines):
+                    # if the line has a quotation
+                    if (lines[index].strip().count("\"") >= 2):
+                        dialogue1 = lines[index].strip()
+                        temp_pairs.append(dialogue1)
+                        # find the next possible line that has dialogue
+                        # for use of counting the lines of writing in between dialouge
+                        in_between = 0 
+                        for i, line2 in enumerate(lines):
+                            if (i > index) and (in_between < 3):
+                                if lines[i].strip().count("\"") >= 2:
+                                    dialogue2 = lines[i].strip()
+                                    temp_pairs.append(dialogue2)
+                                    pairs_count += 1
+                                    pairs.append([dialogue1, dialogue2])
+                    
+                                    #modeled dialogue, response, and the rest of the data
+                                    the_url = "https://archiveofourown.org/works/" + str(row["work_id"])
+                                    pairs_meta.append([num, dialogue1, dialogue2, the_url, hash(dialogue1 + dialogue2), pairs_count, row["work_id"], row["title"],row["author"], row["fandom"], row["words"], row["category"]])
+                                    num += 1
+                                    break 
+                                else:
+                                    in_between += 1 
 
-            # remove whitespaces to allow for easy comparison of lines
-            clean_text = filename + "_clean"
-            with open(filename, 'r') as r, open (clean_text, 'w') as o:
-                for line in r:
-                    if line.strip():
-                        o.write(line)
-            
+            for i in pairs_meta:
+                if i[5] == row["work_id"]:
+                    i[4] = pairs_count
 
-            # read line by line and check for dialogue pairs
-            with open(clean_text, 'r') as file:
-                # save lines to a list
-                lines = file.readlines()
-                temp_pairs = ["temp"]
-                pairs_count = -1
-                for index, line in enumerate(lines):
-                    # make sure lines are in bounds 
-                    if index + 1 < len(lines):
-                        # if the line has a quotation
-                        # if pairs_count == 100:
-                        #     break
-                        if (lines[index].strip().count("\"") >= 2):
-                            # for use of not grabbing duplicates
-                            #in_list = 0
-                            # try:
-                            #     a = temp_pairs.index(lines[index].strip())
-                            #     #print(a)
-                            # except ValueError:
-                            #     in_list = 0
-                            
-                            #if in_list == 0:
-                                dialogue1 = lines[index].strip()
-                                temp_pairs.append(dialogue1)
-                                # find the next possible line that has dialogue
-                                in_between = 0 # for use of counting the lines of writing in between dialouge
-                                for i, line2 in enumerate(lines):
-                                    #if (i > index) and (in_between < 5): #DEBUG - JESS
-                                    if (i > index) and (in_between < 3): # HERE is where we will add our in_between variable. Ex: if (i > index) and (in_between <= 4). Will have to add another if statement if in_between breaches our limit
-                                        if lines[i].strip().count("\"") >= 2:
-                                            # for use of not grabbing duplicates
-                                            # in_list_2 = 1
-                                            # try:
-                                            #     a = temp_pairs.index(lines[i].strip())
-                                            #     #print(a)
-                                            # except ValueError:
-                                            #     in_list_2 = 0
-
-                                            # if in_list_2 == 0:
-                                                dialogue2 = lines[i].strip()
-                                                temp_pairs.append(dialogue2)
-                                                pairs_count += 1
-
-                                                #if row["work_id"] == 14857223: 
-                                                #    print("Dialogue 1: " + dialogue1)
-                                                #    #print("In Between: " + str(in_between))
-                                                #    print("Dialogue 2: " + dialogue2)
-
-                                                pairs.append([dialogue1, dialogue2])
-                                
-                                                # convert author column from list representation to actual list (removes ['author_name'])
-                                                # = re.sub(r"[\[\]\'\"]", "", row["author"])
-
-                                                #add dialogues and meta_data to pairs_meta 
-                                                # meta_data = [row["work_id"], authors, row["rating"], row["category"], 
-                                                #                 row["fandom"], row["relationship"], row["character"], 
-                                                #                 row["additional tags"], row["language"], row["published"], 
-                                                #                 row["status"], row["status date"], row["words"], row["chapters"], 
-                                                #                 row["comments"], row["kudos"], row["bookmarks"], row["hits"], 
-                                                #                 row["all_kudos"]]
-                                                #modeled dialogue, response, and the rest of the data
-                                                the_url = "https://archiveofourown.org/works/" + str(row["work_id"])
-                                                
-                                                #pairs_meta.append([num, dialogue1, dialogue2, the_url, hash(dialogue1 + dialogue2), pairs_count] + meta_data)
-                                                #pairs_meta.append([num, dialogue1, dialogue2, the_url, hash(dialogue1 + dialogue2), pairs_count, row["work_id"]])
-                                                pairs_meta.append([num, dialogue1, dialogue2, the_url, hash(dialogue1 + dialogue2), pairs_count, row["work_id"], row["title"],row["author"], row["fandom"], row["words"], row["category"]])
-                                                num += 1
-                                                break 
-                                        else:
-                                            in_between += 1 
-                
-                for i in pairs_meta:
-                    if i[5] == row["work_id"]:
-                        i[4] = pairs_count
-
-                #num_pairs.append([int(row["words"].replace(",", "")), pairs_count])
-
-            #clean up files
-            os.remove(filename)
-            os.remove(clean_text)
-
- 
+        #clean up files
+        os.remove(filename)
+        os.remove(clean_text)
 
     return pairs, pairs_meta
 
-# 22327
+# The create_csv function takes the pairs and pairs_meta lists from the above function and creates new CSVs for this data.
+# Notes: This also creates Excel files to make them more readable.
+# Dependencies: Reads the pairs and pairs_meta lists from the above function and creates the 'dialogue-pairs.csv' and 'meta-pairs.csv' functions. 
+# Arguments: Pairs and meta from the returned lists in the above function.
+
 def create_csv(pairs, meta):
 
     csv_path = "dialogue-pairs.csv"
@@ -179,24 +125,22 @@ def create_csv(pairs, meta):
     # note: you can't generate an xlsx when the file is open b/c it needs to update. 
     with pd.ExcelWriter('dialogue-pairs.xlsx') as writer:
         df_pairs.to_excel(writer, sheet_name='Pairs', index=False)
-        #df_pairs_meta.to_excel(writer, sheet_name='Pairs and Metadata', index=False)
 
     with pd.ExcelWriter('meta-pairs.xlsx') as writer:
         df_pairs_meta.to_excel(writer, sheet_name='Pairs and Metadata', index=False)
 
     print(".xlsx created successfully")
 
+# The main function executes the get_pairs and create_csv methods, creating a final 'dialogue-pairs.csv' and 'meta-pairs.csv'.
+# Dependencies: Reads the pairs and pairs_meta lists from the above function and creates the 'dialogue-pairs.csv' and 'meta-pairs.csv' functions. 
+# Arguments: Pairs and meta from the returned lists in the above function.
 
 def main():
 
-    body_path = "notmaturecorrect.csv" #work id, body 
-    meta_path = "notmaturecorrect_meta.csv" #work id, url
+    body_path = "notmaturecorrect.csv"
+    meta_path = "notmaturecorrect_meta.csv"
     pairs, meta = get_pairs(body_path, meta_path)
 
-    # for key in pairs:
-    #     print(key, pairs[key])
-
-    #create new csv
     create_csv(pairs, meta)
     print("CSV updated successfully")  
 
